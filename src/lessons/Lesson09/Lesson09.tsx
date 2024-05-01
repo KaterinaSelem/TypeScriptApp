cc7f0e7c800af8686f48fab218b0f272870442b8
KaterinaS. <153716458+KaterinaSelem@users.noreply.github.com>
2024-05-01 17:05:30 +0200

    new changes

--- src/lessons/Lesson09/Lesson09.tsx
+++ src/lessons/Lesson09/Lesson09.tsx
@@ -1,31 +1,58 @@
 import { ChangeEvent, useEffect, useState } from 'react';
-import {
-  InputExample,
-  Lesson09Component,
-  Result
-} from './styles';
+import { InputExample, Lesson09Component, Result } from './styles';
 
 function Lesson09() {
   const [inputValue, setInputValue] = useState<string>('');
+  const [quote, setQuote] = useState<string>('');
 
   const onChangeInput = (event: ChangeEvent<HTMLInputElement>) => {
-    setInputValue(event.target.value)
-  }
-  useEffect(()=>{
-    console.log("Mounting");
-  }, [] );
+    setInputValue(event.target.value);
+  };
 
-  useEffect(()=>{
-    console.log("Update");
-  }, [inputValue] );
+  const getActivity = async () => {
+    try {
+      const responce = await fetch(
+        'https://api.breakingbadquotes.xyz/v1/quotes'
+      );
+      const result = await responce.json();
+      console.log(result[0].quote);
+      if (!responce.ok) {
+        throw Object.assign(new Error('API Error'), {
+          responce: result,
+        });
+      } else {
+        setQuote(result[0].quote);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
+  // getActivity();
+
+  useEffect(() => {
+    console.log('Mounting');
+    getActivity();
+  }, []);
+
+  useEffect(() => {
+    console.log('Update');
+    if (quote) {
+      getActivity();
+    }
+  }, [inputValue]);
 
   return (
     <Lesson09Component>
-      <InputExample name='example' placeholder='example text' onChange={onChangeInput} />
-     <Result>{inputValue}</Result> 
+      <InputExample
+        name='example'
+        placeholder='example text'
+        onChange={onChangeInput}
+      />
+      <Result>{inputValue}</Result>
+      <Result>{quote}</Result>
     </Lesson09Component>
-  )
+  );
 }
 
-export default Lesson09;       
+export default Lesson09;
