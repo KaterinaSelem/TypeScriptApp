1363e95bcd23eec13aab4bb05bef4e9df672255f
KaterinaS. <153716458+KaterinaSelem@users.noreply.github.com>
2024-05-02 23:38:50 +0200

    homework12

--- src/components/EmployeeForm/EmployeeForm.tsx
+++ src/components/EmployeeForm/EmployeeForm.tsx
@@ -1,4 +1,5 @@
 import { useFormik } from 'formik';
+import * as Yup from 'yup';
 import Input from 'components/Input/Input';
 import {
   Checkbox,
@@ -6,63 +7,84 @@ import {
   CheckboxLabel,
   EmployeeFormComponent,
 } from './styles';
-import { EmployeeFormValues } from './types';
+import { EMPLOYEE_FIELD_NAMES, EmployeeFormValues } from './types';
 import Button from 'components/Button/Button';
 
 function EmployeeForm() {
+  const scheme = Yup.object().shape({
+    [EMPLOYEE_FIELD_NAMES.NAME]: Yup.string()
+      .required('Field name is required!')
+      .max(2, 'Max 2 symbols')
+      .min(50, 'Min 50 symbols'),
+    [EMPLOYEE_FIELD_NAMES.SURNAME]: Yup.string()
+      .required('Field surname is required!')
+      .max(15, 'Max 15 symbols'),
+    [EMPLOYEE_FIELD_NAMES.AGE]: Yup.number()
+      .typeError('Age must be number')
+      .required('Field age is required!')
+      .max(3, 'Max 3 symbols')
+      .min(1, 'Min 1 symbols'),
+    [EMPLOYEE_FIELD_NAMES.POSITION]: Yup.string().max(30, 'Max 30 symbols'),
+    [EMPLOYEE_FIELD_NAMES.AGREEMENT]: Yup.boolean(),
+  });
+
   const formik = useFormik({
     initialValues: {
-      name: '',
-      surname: '',
-      age: '',
-      position: '',
-      agreement: false,
+      [EMPLOYEE_FIELD_NAMES.NAME]: '',
+      [EMPLOYEE_FIELD_NAMES.SURNAME]: '',
+      [EMPLOYEE_FIELD_NAMES.AGE]: '',
+      [EMPLOYEE_FIELD_NAMES.POSITION]: '',
+      [EMPLOYEE_FIELD_NAMES.AGREEMENT]: false,
     } as EmployeeFormValues,
+    validationSchema: scheme,
+    validateOnChange: false,
     onSubmit: (values: EmployeeFormValues) => {
       console.log(values);
     },
   });
+
   return (
-    <EmployeeFormComponent onSubmit={formik.handleSubmit}>
+    <EmployeeFormComponent onSubmit={formik.handleSubmit}
+    >
       <Input
-        name='name'
+        name={EMPLOYEE_FIELD_NAMES.NAME}
         placeholder='Enter your name'
         label='Name'
         onInputChange={formik.handleChange}
-        value={formik.values.name}
+        value={formik.values[EMPLOYEE_FIELD_NAMES.NAME]}
       />
       <Input
-        name='surname'
+        name={EMPLOYEE_FIELD_NAMES.SURNAME}
         placeholder='Enter your surname'
         label='Surname'
         onInputChange={formik.handleChange}
-        value={formik.values.surname}
+        value={formik.values[EMPLOYEE_FIELD_NAMES.SURNAME]}
       />
       <Input
-        name='age'
+        name={EMPLOYEE_FIELD_NAMES.AGE}
         placeholder='Enter your age'
         label='Age'
         onInputChange={formik.handleChange}
-        value={formik.values.age}
+        value={formik.values[EMPLOYEE_FIELD_NAMES.AGE]}
       />
       <Input
-        name='position'
+        name={EMPLOYEE_FIELD_NAMES.POSITION}
         placeholder='Enter your position'
         label='Position'
         onInputChange={formik.handleChange}
-        value={formik.values.position}
+        value={formik.values[EMPLOYEE_FIELD_NAMES.POSITION]}
       />
       <CheckboxContainer>
         <Checkbox
           id='agreement-id'
-          name='agreement'
+          name={EMPLOYEE_FIELD_NAMES.AGREEMENT}
           type='checkbox'
           onChange={formik.handleChange}
-          checked={formik.values.agreement}
+          checked={formik.values[EMPLOYEE_FIELD_NAMES.AGREEMENT]}
         />
         <CheckboxLabel htmlFor='agreement-id'>I agree!</CheckboxLabel>
       </CheckboxContainer>
-      <Button type='submit' name='CREATE' disabled={!formik.values.agreement}/>
+      <Button type='submit' name='CREATE' disabled={!formik.values.agreement} />
     </EmployeeFormComponent>
   );
 }
